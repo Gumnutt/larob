cf6a65a07e4f85df47269145d37ba0fb6bac9fa6
Brynn Briedis <brynn@pragma.com.au>
2024-04-24 14:35:15 +1000

    adding args to component

--- web/themes/custom/boral/components/button/button.stories.js
+++ web/themes/custom/boral/components/button/button.stories.js
@@ -1,17 +1,28 @@
+// stories/Button.stories.js
+// Button will be a Javascript function that accepts variables for the twig template.
 import Button from "./button.twig"
+// Import stylesheets, this could be a sass or postcss file too.
 import "./button.css"
-
+// You may also have JavaScript for the component.
 export default {
-  title: "components/button",
+  title: "Components/Button",
   tags: ["autodocs"],
-  component: Button,
-  parameters: {
-    layout: "centered",
+  argTypes: {
+    linkText: {
+      control: { type: "text" },
+    },
+    buttonType: {
+      control: { type: "select" },
+      options: ["primary", "secondary", "tertiary"],
+    },
   },
+  // Just pass along the imported variable.
+  component: Button,
 }
-
+// Set default variables in the story.
 export const Default = {
-  linkText: "My button",
-  link: "#",
-  buttonType: "mega",
+  args: { linkText: "Click me" },
+}
+export const Primary = {
+  args: { linkText: "Click me", buttonType: "primary" },
 }
