1afef6dc8ef396934d05a58355a753a6c187c804
Alex M <alex.monaghan@pragma.com.au>
2024-05-02 16:04:02 +1000

    twig/pcss icons

--- web/themes/custom/boral/vite.config.js
+++ web/themes/custom/boral/vite.config.js
@@ -1,75 +1,84 @@
-import fs from "fs"
-import YAML from "yaml"
-import path from "path"
-import { defineConfig, loadEnv, normalizePath } from "vite"
-import postcss from "postcss"
-import autoprefixer from "autoprefixer"
-import postcssImport from "postcss-import"
-import eslint from "vite-plugin-eslint"
-import twig from "vite-plugin-twig-drupal"
-import { join } from "node:path"
-import liveReload from "vite-plugin-live-reload"
-import { viteExternalsPlugin } from "vite-plugin-externals"
-import { glob } from "glob"
-import postcssNesting from "postcss-nesting"
-import tailwindcss from "tailwindcss"
+import fs from 'fs'
+import YAML from 'yaml'
+import path from 'path'
+import { defineConfig, loadEnv, normalizePath } from 'vite'
+import postcss from 'postcss'
+import autoprefixer from 'autoprefixer'
+import postcssImport from 'postcss-import'
+import eslint from 'vite-plugin-eslint'
+import twig from 'vite-plugin-twig-drupal'
+import { join } from 'node:path'
+import liveReload from 'vite-plugin-live-reload'
+import { viteExternalsPlugin } from 'vite-plugin-externals'
+import { glob } from 'glob'
+import postcssNesting from 'postcss-nesting'
+import tailwindcss from 'tailwindcss'
+import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap'
 
 const { readFile, writeFile } = fs.promises
 
 // Resolve dirs.
-const pwd = path.resolve(__dirname, ".")
-const drupalPath = path.resolve(__dirname, "../../../../")
+const pwd = path.resolve(__dirname, '.')
+const drupalPath = path.resolve(__dirname, '../../../../')
 const themePath = pwd.match(/\/themes\/[^\/]+\/[^\/]+/i)
-const basePath = `${themePath ? themePath[0] : ""}/dist/`
+const basePath = `${themePath ? themePath[0] : ''}/dist/`
 
 // Extract YML as object.
-const yaml = (filename) => {
-  const path = fs.readdirSync(pwd).find((fn) => fn.endsWith(filename))
-  return YAML.parse(fs.readFileSync(path, "utf8"))
+const yaml = filename => {
+  const path = fs.readdirSync(pwd).find(fn => fn.endsWith(filename))
+  return YAML.parse(fs.readFileSync(path, 'utf8'))
 }
 
 // Find any library with vite: true
-const librariesYaml = yaml(".libraries.yml")
+const librariesYaml = yaml('.libraries.yml')
 const librariesInput = Object.keys(librariesYaml)
-  .filter((v) => librariesYaml[v].vite)
-  .map((v) => [...Object.keys(librariesYaml[v].css?.theme), ...Object.keys(librariesYaml[v].js)])
+  .filter(v => librariesYaml[v].vite)
+  .map(v => [
+    ...Object.keys(librariesYaml[v].css?.theme),
+    ...Object.keys(librariesYaml[v].js),
+  ])
   .flat()
-  .filter((v) => v.match(/^assets/))
+  .filter(v => v.match(/^assets/))
 
 // Find any CSS for ckeditor to statically map.
-const infoYaml = yaml(".info.yml")
-const ckeditorInput = (infoYaml["ckeditor5-stylesheets"] || [])
-  .filter((v) => v.match(/\.css$/))
-  .map((v) => v.replace(".css", ".pcss"))
-  .map((v) => v.replace("dist/assets", "assets/pcss"))
+const infoYaml = yaml('.info.yml')
+const ckeditorInput = (infoYaml['ckeditor5-stylesheets'] || [])
+  .filter(v => v.match(/\.css$/))
+  .map(v => v.replace('.css', '.pcss'))
+  .map(v => v.replace('dist/assets', 'assets/pcss'))
 
 // Statically rename the output file.
 const outputMap = {}
-ckeditorInput.forEach((v) => {
-  outputMap[path.basename(v).replace(".pcss", ".css")] = "assets/[name].[ext]"
+ckeditorInput.forEach(v => {
+  outputMap[path.basename(v).replace('.pcss', '.css')] = 'assets/[name].[ext]'
 })
 
 // SDC theming
 function drupalSDC() {
   return {
-    name: "drupal-sdc",
-    enforce: "pre",
+    name: 'drupal-sdc',
+    enforce: 'pre',
     async handleHotUpdate({ file, server }) {
       // Check if the changed file is a .pcss file
-      if (file.includes("components") && file.endsWith(".pcss")) {
+      if (file.includes('components') && file.endsWith('.pcss')) {
         console.log(`Recompiling ${file}`)
         try {
-          const css = await readFile(file, "utf-8")
-          const result = await postcss([postcssImport(), postcssNesting(), tailwindcss(), autoprefixer()]).process(css, { from: undefined })
+          const css = await readFile(file, 'utf-8')
+          const result = await postcss([
+            postcssImport(),
+            postcssNesting(),
+            tailwindcss(),
+            autoprefixer(),
+          ]).process(css, { from: undefined })
 
-          const cssFileName = file.replace(".pcss", ".css")
+          const cssFileName = file.replace('.pcss', '.css')
           await writeFile(cssFileName, result.css)
           console.log(`Updated: ${cssFileName}`)
           server.hot.send({
-            type: "full-reload",
+            type: 'full-reload',
             updates: [
               {
-                type: "update", // use 'js-update' to trigger a reload of the module
+                type: 'update', // use 'js-update' to trigger a reload of the module
                 path: normalizePath(cssFileName),
                 acceptedPath: normalizePath(cssFileName),
                 timestamp: Date.now(),
@@ -86,38 +95,51 @@ function drupalSDC() {
 }
 
 export default ({ mode }) => {
-  const env = loadEnv(mode, drupalPath, "")
+  const env = loadEnv(mode, drupalPath, '')
 
   const config = {
     plugins: [
       // eslint({
       //   exclude: [/virtual:/, /node_modules/, /sb-preview/],
       // }),
-      twig({
-        namespaces: {
-          components: join(__dirname, "components"),
+      VitePluginSvgSpritemap('./assets/icons/*.svg', {
+        prefix: 'icon-',
+        output: {
+          filename: '[name][extname]',
+          name: 'icons.svg',
+          view: false,
+          use: true,
+        },
+        styles: {
+          filename: 'assets/pcss/spritemap.pcss',
+          lang: 'pcss',
         },
       }),
-      liveReload(__dirname + "/**/*.(php|theme|twig|module)"),
+      twig({
+        namespaces: {
+          components: join(__dirname, 'components'),
+        },
+      }),
+      liveReload(__dirname + '/**/*.(php|theme|twig|module)'),
       viteExternalsPlugin({
-        jquery: "jQuery",
-        Drupal: "Drupal",
-        once: "once",
-        drupalSettings: "drupalSettings",
+        jquery: 'jQuery',
+        Drupal: 'Drupal',
+        once: 'once',
+        drupalSettings: 'drupalSettings',
       }),
       drupalSDC(),
     ],
 
-    base: mode === "development" ? "/" : basePath,
+    base: mode === 'development' ? '/' : basePath,
 
     build: {
-      outDir: "dist",
+      outDir: 'dist',
       manifest: true,
       rollupOptions: {
         input: [...librariesInput, ...ckeditorInput],
         output: {
-          assetFileNames: (assetInfo) => {
-            return outputMap[assetInfo.name] || "assets/[name].[hash].[ext]"
+          assetFileNames: assetInfo => {
+            return outputMap[assetInfo.name] || 'assets/[name].[hash].[ext]'
           },
         },
       },
@@ -149,9 +171,9 @@ export default ({ mode }) => {
     proxyTarget = env.DDEV_PRIMARY_URL
   }
 
-  if (proxyTarget && mode === "development") {
+  if (proxyTarget && mode === 'development') {
     config.server.proxy = {
-      "^/(system|api|jsonapi|graphql|oauth)/.*": {
+      '^/(system|api|jsonapi|graphql|oauth)/.*': {
         target: proxyTarget,
         changeOrigin: true,
       },
